e491a2bcc096c57597ac44cef1d1ccf171d0db88
abigailwambui <abigailw15njuguna@gmail.com>
2019-04-29 19:03:39 +0300

    add inputs to make highlight directive function

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) {}
+  constructor(private elem:ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('grey');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+  private highlight(color:string) {
+    this.elem.nativeElement.style.backgroundColor = color;
+  }
 
 }
